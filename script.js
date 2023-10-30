81aa87f396caa35275eb40b210755b245e25c0cf
jahnavreddy <144227880+jahnavreddy@users.noreply.github.com>
2023-10-30 15:20:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+var x = 5;
+var y = 2;
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+var remainder = x%y;
+console.log(remainder)
+console.log(x**y)
+var z = 20;
+console.log(20-1)
+var a =20;
+var b = 10;
+var c =15;
+var d =20;
+a+=b;
+console.log(a)
+
+b -=d;
+console.log(b)
+
+c *= a;
+console.log(c)
+
+d/=b
+console.log(d)
+
+console.log(a==a)
+console.log(a==b)
+
+console.log(a!=b)
+console.log(a==b)
+
+console.log(a!=b)
+console.log(a!=c)
+
+console.log(a<b)
+console.log(a<b)
+
+console.log(a>b)
+console.log(a>c)
+
+console.log(a>=b)
+console.log(a>=c)
+
+console.log(a<=b)
+console.log(a<=c)
